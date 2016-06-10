bcb3d6f7aaeb96c2ef3b44ccb43f995396c22c55
shane preston <spreston2009@gmail.com>
2016-06-10 18:33:29 +0100

    appends paths to .txt file

---
+++ opendir.py
@@ -0,0 +1,24 @@
+import os
+# only works in linux
+# user needs to be root for script to work (sudo -i) in the terminal
+# if the file opendir.txt already exists
+# remove it
+if os.path.exists('./opendir.txt'):
+	os.remove('./opendir.txt')
+	print "file removed"
+
+# open all directories and subdirectories and print out the path for each file found
+# to only open dirs and files for current directory replace "/" with "."
+for root, dirs, files in os.walk("/", topdown=False):
+    for name in files:
+	filepath = (os.path.join(root, name))
+        print filepath
+	#writes to file
+	with open("opendir.txt", "a") as myfile:
+		myfile.write(filepath + "\n")
+    for name in dirs:
+	dirpath = (os.path.join(root, name))
+        print dirpath
+	#writes to file
+	with open("opendir.txt", "a") as myfile:
+		myfile.write(dirpath + "\n")
