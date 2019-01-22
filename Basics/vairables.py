e2fcd92299224c7bc4f644a596d23488d4ea8b0c
shane preston <spreston2009@gmail.com>
2019-01-22 03:31:30 +0000

    Update vairables.py

--- Basics/vairables.py
+++ Basics/vairables.py
@@ -1,1 +1,38 @@
-# nothing yet
+# Variable Declaration
+# (will work in all versions of python)
+
+# Assigning values to variables
+i = 10                          # assigns integer value "10" to variable "i"
+f = 2.5                         # assigns floating point (decimal) value "2.5" to variable "f"
+s = "John"                      # assigns string value "John" to variable "s"
+
+# Multiple variable assignment
+a = b = c = 1                   # assigns value "1" to all three variables (a,b & c)
+a, b, c = 1, 2, "John"          # assigns value "1" to variable "a", value "2" to variable "b" & value "John" to variable "c"
+
+# Variable data types
+
+# Numbers
+# Strings
+# Lists
+# Tuples
+# Dictionaries
+
+# Data type conversion functions
+
+int()                           # converts value to an integer
+float()                         # converts value to a floating point (decimal)
+complex()                       # creates a complex number from values
+str()                           # converts value to a string
+repr()	                        # converts value to expression string
+eval()	                        # evaluates string value and returns an object
+tuple()                         # converts value to a tuple
+list()                          # converts value to a list
+set()                           # converts value to a set
+dict()	                        # converts value to a dictionary
+frozenset()
+chr()                           # converts value to a char
+unichr()	
+ord()	
+hex()
+oct()
